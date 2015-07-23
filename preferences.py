2d11a1019c799b5d591ee2d318b0f4b461d93820
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-07-23 10:54:35 -0400

    Tabs changed to spaces

--- preferences.py
+++ preferences.py
@@ -2,54 +2,54 @@ import numpy, theano
 from theano import tensor as T
 
 class PreferenceModel(object):
-	def __init__(self, pref_type, modelname, hidden_size, ont_rep, vocab_rep=None):
-		"""
-		pref_type (string):	Indicate whether the preference being modeled is word-concept or concept-concept (word_concept, concept_concept)
-		modelname (string):	Select the parameterization of the model ('lincomb')
-		ont_rep (theano.shared):	Shared ontology representation
-		vocab_rep (theano.shared):	Shared vocabulary representation (required if pref_type is word-concept)
-		"""
-		if pref_type == 'word_concept':
-			self.pref_type = pref_type
-			if vocab_rep is None:
-				raise RuntimeError, "vocab_rep has to be given when pref_type is word_concept"
-		else:
-			self.pref_type = 'concept_concept'
-		impl_models = ['linlayer']
-		if modelname not in impl_models:
-			raise NotImplementedError, "Model name %s not known"%(modelname)
-		numpy_rng = numpy.random.RandomState(12345)
-		self.modelname = modelname
-		self.vocab_rep = vocab_rep
-		self.ont_rep = ont_rep
-		ont_size, concept_dim = ont_rep.get_value().shape
-		if self.pref_type == 'word_concept':
-			vocab_size, word_dim = vocab_rep.get_value().shape
-			ent1_dim = word_dim
-			ent2_dim = concept_dim
-		else:
-			ent1_dim = concept_dim
-			ent2_dim = concept_dim
+  def __init__(self, pref_type, modelname, hidden_size, ont_rep, vocab_rep=None):
+    """
+    pref_type (string):  Indicate whether the preference being modeled is word-concept or concept-concept (word_concept, concept_concept)
+    modelname (string):  Select the parameterization of the model ('lincomb')
+    ont_rep (theano.shared):  Shared ontology representation
+    vocab_rep (theano.shared):  Shared vocabulary representation (required if pref_type is word-concept)
+    """
+    if pref_type == 'word_concept':
+      self.pref_type = pref_type
+      if vocab_rep is None:
+        raise RuntimeError, "vocab_rep has to be given when pref_type is word_concept"
+    else:
+      self.pref_type = 'concept_concept'
+    impl_models = ['linlayer']
+    if modelname not in impl_models:
+      raise NotImplementedError, "Model name %s not known"%(modelname)
+    numpy_rng = numpy.random.RandomState(12345)
+    self.modelname = modelname
+    self.vocab_rep = vocab_rep
+    self.ont_rep = ont_rep
+    ont_size, concept_dim = ont_rep.get_value().shape
+    if self.pref_type == 'word_concept':
+      vocab_size, word_dim = vocab_rep.get_value().shape
+      ent1_dim = word_dim
+      ent2_dim = concept_dim
+    else:
+      ent1_dim = concept_dim
+      ent2_dim = concept_dim
 
-		if self.modelname == 'linlayer':
-			proj_weight_dim = ent1_dim + ent2_dim
-			proj_weight_range = 4 * numpy.sqrt(6. / (proj_weight_dim + hidden_size))
-			init_proj_weight = numpy.asarray(numpy_rng.uniform(low = -proj_weight_range, high = proj_weight_range, size = (proj_weight_dim, hidden_size)))
-			self.proj_weight = theano.shared(value=init_proj_weight, name='P_p')
-			score_weight_range = 4 * numpy.sqrt(6. / hidden_size)
-			init_score_weight = numpy.asarray(numpy_rng.uniform(low = -score_weight_range, high = score_weight_range, size = hidden_size))
-			self.score_weight = theano.shared(value=init_score_weight, name='p_s')
-			self.params = [self.proj_weight, self.score_weight]
+    if self.modelname == 'linlayer':
+      proj_weight_dim = ent1_dim + ent2_dim
+      proj_weight_range = 4 * numpy.sqrt(6. / (proj_weight_dim + hidden_size))
+      init_proj_weight = numpy.asarray(numpy_rng.uniform(low = -proj_weight_range, high = proj_weight_range, size = (proj_weight_dim, hidden_size)))
+      self.proj_weight = theano.shared(value=init_proj_weight, name='P_p')
+      score_weight_range = 4 * numpy.sqrt(6. / hidden_size)
+      init_score_weight = numpy.asarray(numpy_rng.uniform(low = -score_weight_range, high = score_weight_range, size = hidden_size))
+      self.score_weight = theano.shared(value=init_score_weight, name='p_s')
+      self.params = [self.proj_weight, self.score_weight]
 
-	def get_symb_score(self, ent1_ind, ent2_ind):
-		if self.pref_type == 'word_concept':
-			ent1_vec = self.vocab_rep[ent1_ind]
-		else:
-			ent1_vec = self.ont_rep[ent1_ind]
-		ent2_vec = self.ont_rep[ent2_ind]
-		pref_score = T.dot( self.score_weight, T.dot(self.proj_weight.T, T.concatenate([ent1_vec, ent2_vec])))
-		return pref_score
+  def get_symb_score(self, ent1_ind, ent2_ind):
+    if self.pref_type == 'word_concept':
+      ent1_vec = self.vocab_rep[ent1_ind]
+    else:
+      ent1_vec = self.ont_rep[ent1_ind]
+    ent2_vec = self.ont_rep[ent2_ind]
+    pref_score = T.dot( self.score_weight, T.dot(self.proj_weight.T, T.concatenate([ent1_vec, ent2_vec])))
+    return pref_score
 
-	def get_params(self):
-		return self.params
+  def get_params(self):
+    return self.params
 
--- reconstruction.py
+++ reconstruction.py
@@ -2,26 +2,26 @@ import numpy, theano
 from theano import tensor as T
 
 class ReconstructionModel(object):
-	def __init__(self, ont_size, vocab_rep):
-		"""
-		ont_size (int):	Number of concepts in the ontology
-		word_dim (int):	Dimensionality of word representations
-		"""
-		numpy_rng = numpy.random.RandomState(12345)
-		_, word_dim = vocab_rep.get_value().shape
-		self.vocab_rep = vocab_rep
-		avg_range = 4 * numpy.sqrt(6. / (ont_size + word_dim))
-		init_avgs = numpy.asarray(numpy_rng.uniform(low = -avg_range, high = avg_range, size = (ont_size, word_dim)))
-		self.avgs = theano.shared(value = init_avgs, name = 'avgs')
-		cov_range = 4 * numpy.sqrt(6. / (ont_size + word_dim + word_dim))
-		init_covs = numpy.asarray(numpy_rng.uniform(low = -cov_range, high = cov_range, size = (ont_size, word_dim, word_dim)))	
-		self.covs = theano.shared(value = init_covs, name = 'covs')
+  def __init__(self, ont_size, vocab_rep):
+    """
+    ont_size (int):  Number of concepts in the ontology
+    word_dim (int):  Dimensionality of word representations
+    """
+    numpy_rng = numpy.random.RandomState(12345)
+    _, word_dim = vocab_rep.get_value().shape
+    self.vocab_rep = vocab_rep
+    avg_range = 4 * numpy.sqrt(6. / (ont_size + word_dim))
+    init_avgs = numpy.asarray(numpy_rng.uniform(low = -avg_range, high = avg_range, size = (ont_size, word_dim)))
+    self.avgs = theano.shared(value = init_avgs, name = 'avgs')
+    cov_range = 4 * numpy.sqrt(6. / (ont_size + word_dim + word_dim))
+    init_covs = numpy.asarray(numpy_rng.uniform(low = -cov_range, high = cov_range, size = (ont_size, word_dim, word_dim)))  
+    self.covs = theano.shared(value = init_covs, name = 'covs')
 
-	def get_sym_rec_prob(self, word_ind, concept_ind):
-		avg, cov = self.avgs[concept_ind], self.covs[concept_ind]
-		word_rep = self.vocab_rep[word_ind]
-		self.p_r = 1. / T.sqrt((2 * numpy.pi) ** 2 * T.nlinalg.det(cov)) * T.exp(- 0.5 * T.dot(T.dot((word_rep - avg), T.nlinalg.matrix_inverse(cov)), (word_rep - avg) ))
-		return self.p_r
+  def get_sym_rec_prob(self, word_ind, concept_ind):
+    avg, cov = self.avgs[concept_ind], self.covs[concept_ind]
+    word_rep = self.vocab_rep[word_ind]
+    self.p_r = 1. / T.sqrt((2 * numpy.pi) ** 2 * T.nlinalg.det(cov)) * T.exp(- 0.5 * T.dot(T.dot((word_rep - avg), T.nlinalg.matrix_inverse(cov)), (word_rep - avg) ))
+    return self.p_r
 
-	def get_params(self):
-		return [self.avgs, self.covs]
+  def get_params(self):
+    return [self.avgs, self.covs]
