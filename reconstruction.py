d622dc9f6b862f7a87e4bd02a8e61c84e09c4698
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-08-29 00:20:23 -0400

    Removed comments

--- reconstruction.py
+++ reconstruction.py
@@ -15,23 +15,19 @@ class ReconstructionModel(object):
     avg_range = 4 * numpy.sqrt(6. / (ont_size + word_dim))
     init_avgs = numpy.asarray(numpy_rng.uniform(low = -avg_range, high = avg_range, size = (ont_size, word_dim)))
     self.avgs = theano.shared(value = init_avgs, name = 'avgs')
-    #cov_range = 4 * numpy.sqrt(6. / (ont_size + word_dim + word_dim))
-    #init_covs = numpy.asarray(numpy_rng.uniform(low = -cov_range, high = cov_range, size = (ont_size, word_dim, word_dim)))  
-    #self.covs = theano.shared(value = init_covs, name = 'covs')
     init_cov_multiples = numpy.asarray(numpy_rng.uniform(size=ont_size))
     self.cov_multiples = theano.shared(value = init_cov_multiples, name = 'cov_mult')
     self.word_dim = word_dim
 
   def get_sym_rec_prob(self, word_ind, concept_ind):
-    #avg, cov = self.avgs[concept_ind], self.covs[concept_ind]
     avg, cov_m = self.avgs[concept_ind], self.cov_multiples[concept_ind]
-    cov = T.eye(self.word_dim) * T.abs_(cov_m)
     word_rep = self.vocab_rep[word_ind]
-    self.p_r = 1. / T.sqrt(((2 * numpy.pi) ** self.word_dim) * T.nlinalg.det(cov)) * T.exp(- 0.5 * T.dot(T.dot((word_rep - avg), T.nlinalg.matrix_inverse(cov)), (word_rep - avg) ))
+    exp_term = -0.5 * T.dot((word_rep - avg), (word_rep - avg)) * T.abs_(cov_m)
+    sqrt_term = T.pow(2 * T.abs_(cov_m) * numpy.pi, self.word_dim)
+    self.p_r = 1. / T.sqrt(sqrt_term) * T.exp(exp_term)
     return self.p_r
 
   def get_params(self):
-    #return [self.avgs, self.covs]
     return [self.avgs, self.cov_multiples]
 
   def set_params(self, params):
