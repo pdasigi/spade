ce080934432de57b195f944004b3c7cbd8a999ab
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-08-30 18:27:17 -0400

    Added functions to set parameters

--- event_ae.py
+++ event_ae.py
@@ -40,6 +40,8 @@ class EventAE(object):
     self.rec_params = self.rec_model.get_params()
     # Random y, sampled from uniform(|ont|^num_slots)
     self.y_r = T.cast(self.theano_rng.uniform(low=0, high=self.ont_size-1, size=(self.num_slots,)), 'int32')
+    self.num_enc_ns = 1
+    self.num_label_ns = 1
   
   def get_sym_rand_y(self, y_s):
     # NCE function
@@ -69,9 +71,11 @@ class EventAE(object):
     encoder_partition = partial_sums[-1]
     return encoder_partition
 
-  def get_sym_nc_encoder_prob(self, x, y, num_noise_samples=5):
+  def get_sym_nc_encoder_prob(self, x, y, num_noise_samples=None):
     # NCE function
     # Noise distribution is not conditioned on x. So we sample directly from ont, not from y_s
+    if num_noise_samples is None:
+      num_noise_samples = self.num_enc_ns
     enc_energy = self.get_sym_encoder_energy(x, y)
     ns_prob = num_noise_samples * (1. / self.ont_size) ** self.num_slots
     true_prob = enc_energy / (enc_energy + ns_prob)
@@ -100,17 +104,21 @@ class EventAE(object):
     posterior_partition = partial_sums[-1]
     return posterior_partition
 
-  def get_sym_nc_posterior(self, x, y, num_noise_samples=5):
+  def get_sym_nc_posterior(self, x, y, num_noise_samples=None):
     # NCE function
     # p(\hat{x}, y | x)
-    return self.get_sym_nc_encoder_prob(x, y, num_noise_samples) * self.get_sym_rec_prob(x, y)
+    if num_noise_samples is None:
+      num_noise_samples = self.num_enc_ns
+    return self.get_sym_nc_encoder_prob(x, y, num_noise_samples=self.num_enc_ns) * self.get_sym_rec_prob(x, y)
 
-  def get_sym_nc_label_prob(self, x, y, y_s, num_noise_samples=5):
+  def get_sym_nc_label_prob(self, x, y, y_s, num_noise_samples=None):
     # NCE function
     # p(y | x, \hat{x})
+    if num_noise_samples is None:
+      num_noise_samples = self.num_label_ns
     true_posterior = self.get_sym_nc_posterior(x, y)
     #TODO: Can make this more efficient
-    noise_posterior = self.get_sym_nc_posterior(x, self.get_sym_rand_y(y_s))
+    noise_posterior = self.get_sym_nc_posterior(x, self.get_sym_rand_y(y_s), num_noise_samples=1)
     ns_prob = num_noise_samples * T.pow(1. / y_s.shape[0], self.num_slots)
     true_prob = true_posterior / (true_posterior + ns_prob)
     noise_prob = T.constant(1.0, dtype='float64')
@@ -137,7 +145,7 @@ class EventAE(object):
     #complete_expectation = data_term
     return complete_expectation
   
-  def get_sym_nc_complete_expectation(self, x, y_s, num_noise_samples=5):
+  def get_sym_nc_complete_expectation(self, x, y_s):
     # NCE function
     def get_expectation(y_0, interm_sum, x_0, Y):
       label_prob = self.get_sym_nc_label_prob(x_0, y_0, Y)
@@ -165,4 +173,12 @@ class EventAE(object):
     x, y = T.ivectors('x', 'y')
     posterior_func = theano.function([x, y], self.get_sym_posterior_num(x, y))
     return posterior_func
+
+  def set_repr_params(self, repr_param_vals):
+    for i, param_val in enumerate(repr_param_vals):
+      self.repr_params[i].set_value(param_val)
+
+  def set_rec_params(self, rec_param_vals):
+    for i, param_val in enumerate(rec_param_vals):
+      self.rec_params[i].set_value(param_val)
 
