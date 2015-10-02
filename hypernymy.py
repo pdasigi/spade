b5b652b769095d6e06d26debac5c6fc3b8dda841
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-10-02 16:55:36 -0400

    Tanh and dot product for hypernymy

--- hypernymy.py
+++ hypernymy.py
@@ -8,7 +8,7 @@ class HypernymModel(object):
     vocab_rep (theano.shared):  Shared vocabulary representation
     ont_rep (theano.shared):  Shared ontology representation
     """
-    impl_models = ['linlayer']
+    impl_models = ['linlayer', 'tanhlayer', 'dotproduct']
     if modelname not in impl_models:
       raise NotImplementedError, "Model name %s not known"%(modelname)
     numpy_rng = numpy.random.RandomState(12345)
@@ -17,21 +17,28 @@ class HypernymModel(object):
     self.ont_rep = ont_rep
     vocab_size, word_dim = vocab_rep.get_value().shape
     ont_size, concept_dim = ont_rep.get_value().shape
-    if self.modelname == 'linlayer':
+    self.params = []
+    if self.modelname == 'linlayer' or self.modelname == 'tanhlayer':
       proj_weight_dim = word_dim + concept_dim
-      proj_weight_range = 4 * numpy.sqrt(6. / (proj_weight_dim + hidden_size))
+      proj_weight_range = numpy.sqrt(6. / (proj_weight_dim + hidden_size))
       init_proj_weight = numpy.asarray(numpy_rng.uniform(low = -proj_weight_range, high = proj_weight_range, size = (proj_weight_dim, hidden_size)))
       self.proj_weight = theano.shared(value=init_proj_weight, name='H_p')
-      score_weight_range = 4 * numpy.sqrt(6. / hidden_size)
+      score_weight_range = numpy.sqrt(6. / hidden_size)
       init_score_weight = numpy.asarray(numpy_rng.uniform(low = -score_weight_range, high = score_weight_range, size = hidden_size))
       self.score_weight = theano.shared(value=init_score_weight, name='h_s')
       self.params = [self.proj_weight, self.score_weight]
 
   def get_symb_score(self, word_ind, concept_ind):
+    word_vec = self.vocab_rep[word_ind]
+    concept_vec = self.ont_rep[concept_ind]
     if self.modelname == 'linlayer':
-      word_vec = self.vocab_rep[word_ind]
-      concept_vec = self.ont_rep[concept_ind]
       self.score = T.dot( self.score_weight, T.dot( self.proj_weight.T,  T.concatenate([word_vec, concept_vec]) ) )
+      return self.score
+    elif self.modelname == 'tanhlayer':
+      self.score = T.dot( self.score_weight, T.tanh(T.dot( self.proj_weight.T,  T.concatenate([word_vec, concept_vec]))))
+      return self.score
+    elif self.modelname == "dotproduct":
+      self.score = T.dot(word_vec, concept_vec)
       return self.score
     else:
       raise NotImplementedError
