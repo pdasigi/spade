615ede7469ef57be8904b30a733c268237575493
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-10-23 16:11:05 -0400

    Added a faster and relaxed variant

--- event_ae.py
+++ event_ae.py
@@ -13,7 +13,7 @@ SMALL_NUM = 1e-30
 LOG_SMALL_NUM = numpy.log(SMALL_NUM)
 
 class EventAE(object):
-  def __init__(self, num_args, vocab_size, ont_size, hyp_hidden_size, wc_hidden_sizes, cc_hidden_sizes, word_dim=50, concept_dim=50, word_rep_param=False, hyp_model_type="weighted_prod", wc_pref_model_type="tanhlayer", cc_pref_model_type="tanhlayer"):
+  def __init__(self, num_args, vocab_size, ont_size, hyp_hidden_size, wc_hidden_sizes, cc_hidden_sizes, word_dim=50, concept_dim=50, word_rep_param=False, hyp_model_type="weighted_prod", wc_pref_model_type="tanhlayer", cc_pref_model_type="tanhlayer", relaxed=False):
     print >>sys.stderr, "Initializing SPADE"
     print >>sys.stderr, "num_args: %d"%(num_args)
     print >>sys.stderr, "vocab_size: %d"%(vocab_size)
@@ -23,7 +23,10 @@ class EventAE(object):
     print >>sys.stderr, "word_rep_param: %s"%(word_rep_param)
     print >>sys.stderr, "hyp_model: %s"%(hyp_model_type)
     print >>sys.stderr, "wc_pref_model: %s"%(wc_pref_model_type)
-    print >>sys.stderr, "cc_pref_model: %s"%(cc_pref_model_type)
+    if relaxed:
+      print >>sys.stderr, "Running without inter-concept preferences"
+    else:
+      print >>sys.stderr, "cc_pref_model: %s"%(cc_pref_model_type)
 
     numpy_rng = numpy.random.RandomState(12345)
     self.theano_rng = RandomStreams(12345)
@@ -37,6 +40,7 @@ class EventAE(object):
     self.repr_params = [self.vocab_rep] if word_rep_param else []
     self.repr_params.append(self.ont_rep)
     self.enc_params = []
+    self.relaxed = relaxed
     self.hyp_model = HypernymModel(hyp_model_type, hyp_hidden_size, self.vocab_rep, self.ont_rep)
     self.enc_params.extend(self.hyp_model.get_params())
     self.wc_pref_models = []
@@ -47,25 +51,20 @@ class EventAE(object):
       wc_pref_model = PreferenceModel('word_concept', wc_pref_model_type, wc_hidden_sizes[i], self.ont_rep, self.vocab_rep)
       self.wc_pref_models.append(wc_pref_model)
       self.enc_params.extend(wc_pref_model.get_params())
-  
-    for i in range(num_args):
-      cc_pref_model = PreferenceModel('concept_concept', cc_pref_model_type, cc_hidden_sizes[i], self.ont_rep)
-      self.cc_pref_models.append(cc_pref_model)
-      self.enc_params.extend(cc_pref_model.get_params())
+
+    if not self.relaxed:  
+      for i in range(num_args):
+        cc_pref_model = PreferenceModel('concept_concept', cc_pref_model_type, cc_hidden_sizes[i], self.ont_rep)
+        self.cc_pref_models.append(cc_pref_model)
+        self.enc_params.extend(cc_pref_model.get_params())
     self.rec_model = ReconstructionModel(self.ont_rep, self.vocab_rep)
     self.rec_params = self.rec_model.get_params()
     # Random y, sampled from uniform(|ont|^num_slots)
     self.y_r = T.cast(self.theano_rng.uniform(low=0, high=self.ont_size-1, size=(self.num_slots,)), 'int32')
     self.num_enc_ns = 1
     self.num_label_ns = 1
-  
-  def get_sym_rand_y(self, y_s):
-    # NCE function
-    # Sample randomly from y|x
-    rand_ind = T.cast(self.theano_rng.uniform(low=0, high=y_s.shape[0]-1, size=(1,)), 'int32')
-    sample = y_s[rand_ind[0]]
-    return sample
-    
+
+  ### Direct prob functions ###
   def get_sym_encoder_energy(self, x, y):
     # Works with NCE
     hsum = T.constant(0)
@@ -87,6 +86,59 @@ class EventAE(object):
     encoder_partition = partial_sums[-1]
     return encoder_partition
 
+  def get_sym_rec_prob(self, x, y):
+    # Works with NCE
+    init_prob = T.constant(1.0, dtype='float64')
+    partial_prods, _ = theano.scan(fn = lambda x_i, y_i, interm_prod: interm_prod * self.rec_model.get_sym_rec_prob(x_i, y_i), outputs_info=init_prob, sequences=[x, y])
+    rec_prob = partial_prods[-1]
+    return rec_prob
+  
+  def get_sym_posterior_num(self, x, y):
+    # Needed for NCE
+    enc_energy = self.get_sym_encoder_energy(x, y)
+    rec_prob = self.get_sym_rec_prob(x, y)
+    return T.exp(enc_energy) * rec_prob
+      
+  def get_sym_posterior_partition(self, x, y_s):
+    partial_sums, _ = theano.scan(fn=lambda y, interm_sum, x_0: interm_sum + self.get_sym_posterior_num(x_0, y), outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=x)
+    posterior_partition = partial_sums[-1]
+    return posterior_partition
+  
+  def get_sym_direct_prob(self, x, y_s):
+    def get_post_num_sum(y_0, interm_sum, x_0):
+      posterior_num = self.get_sym_posterior_num(x_0, y_0)
+      return interm_sum + posterior_num
+    res, _ = theano.scan(fn=get_post_num_sum, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x])
+    direct_prob = res[-1] / self.get_sym_encoder_partition(x, y_s)
+    return direct_prob
+
+  # Following function is useless.
+  def get_sym_posterior(self, x, y, y_s):
+    return self.get_sym_posterior_num(x, y) / self.get_sym_posterior_partition(x, y_s)
+
+  ### Complete expectation functions ###
+  def get_sym_complete_expectation(self, x, y_s):
+    encoder_partition = self.get_sym_encoder_partition(x, y_s)
+    posterior_partition = self.get_sym_posterior_partition(x, y_s)
+    def prod_fun(y_0, interm_sum, x_0): 
+      post_num = self.get_sym_posterior_num(x_0, y_0)
+      fixed_post_num = ifelse(T.le(post_num, SMALL_NUM), T.constant(0.0, dtype='float64'), post_num)
+      return interm_sum + ifelse(T.le(fixed_post_num, SMALL_NUM), T.constant(0.0, dtype='float64'), fixed_post_num * T.log(fixed_post_num))
+    partial_sums, _ = theano.scan(fn=prod_fun, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=x)
+    data_term = ifelse(T.eq(posterior_partition, T.constant(0.0, dtype='float64')), T.constant(0.0, dtype='float64'), partial_sums[-1] / posterior_partition)
+    #data_term = partial_sums[-1]
+    complete_expectation = data_term - T.log(encoder_partition)
+    #complete_expectation = data_term
+    return complete_expectation
+
+  ### NCE functions  ###
+  def get_sym_rand_y(self, y_s):
+    # NCE function
+    # Sample randomly from y|x
+    rand_ind = T.cast(self.theano_rng.uniform(low=0, high=y_s.shape[0]-1, size=(1,)), 'int32')
+    sample = y_s[rand_ind[0]]
+    return sample
+    
   def get_sym_nc_encoder_prob(self, x, y, y_s, num_noise_samples=None):
     # NCE function
     if num_noise_samples is None:
@@ -102,34 +154,21 @@ class EventAE(object):
       noise_prob *= ns_prob / (ns_enc_energy + ns_prob)
     return true_prob * noise_prob
 
-  def get_sym_rec_prob(self, x, y):
-    # Works with NCE
-    init_prob = T.constant(1.0, dtype='float64')
-    partial_prods, _ = theano.scan(fn = lambda x_i, y_i, interm_prod: interm_prod * self.rec_model.get_sym_rec_prob(x_i, y_i), outputs_info=init_prob, sequences=[x, y])
-    rec_prob = partial_prods[-1]
-    return rec_prob
-    
-  def get_sym_posterior_num(self, x, y):
-    # Needed for NCE
-    enc_energy = self.get_sym_encoder_energy(x, y)
-    rec_prob = self.get_sym_rec_prob(x, y)
-    return T.exp(enc_energy) * rec_prob
-      
-  def get_sym_posterior_partition(self, x, y_s):
-    partial_sums, _ = theano.scan(fn=lambda y, interm_sum, x_0: interm_sum + self.get_sym_posterior_num(x_0, y), outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=x)
-    posterior_partition = partial_sums[-1]
-    return posterior_partition
-
-  # Following function is useless.
-  def get_sym_posterior(self, x, y, y_s):
-    return self.get_sym_posterior_num(x, y) / self.get_sym_posterior_partition(x, y_s)
-
   def get_sym_nc_posterior(self, x, y, y_s, num_noise_samples=None):
     # NCE function
     # p(\hat{x}, y | x)
     if num_noise_samples is None:
       num_noise_samples = self.num_enc_ns
     return self.get_sym_nc_encoder_prob(x, y, y_s, num_noise_samples=num_noise_samples) * self.get_sym_rec_prob(x, y)
+
+  def get_sym_nc_direct_prob(self, x, y_s):
+    # NCE function
+    def get_prob(y_0, interm_sum, x_0, Y):
+      posterior = self.get_sym_nc_posterior(x_0, y_0, Y)
+      return interm_sum + posterior
+    res, _ = theano.scan(fn=get_prob, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x, y_s])
+    direct_prob = res[-1]
+    return direct_prob
 
   def get_sym_nc_label_prob(self, x, y, y_s, num_noise_samples=None):
     # NCE function
@@ -146,20 +185,6 @@ class EventAE(object):
       #noise_posterior = self.get_sym_nc_posterior(x, self.get_sym_rand_y(y_s))
       noise_prob *= ns_prob / (noise_posterior + ns_prob)
     return true_prob * noise_prob
-
-  def get_sym_complete_expectation(self, x, y_s):
-    encoder_partition = self.get_sym_encoder_partition(x, y_s)
-    posterior_partition = self.get_sym_posterior_partition(x, y_s)
-    def prod_fun(y_0, interm_sum, x_0): 
-      post_num = self.get_sym_posterior_num(x_0, y_0)
-      fixed_post_num = ifelse(T.le(post_num, SMALL_NUM), T.constant(0.0, dtype='float64'), post_num)
-      return interm_sum + ifelse(T.le(fixed_post_num, SMALL_NUM), T.constant(0.0, dtype='float64'), fixed_post_num * T.log(fixed_post_num))
-    partial_sums, _ = theano.scan(fn=prod_fun, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=x)
-    data_term = ifelse(T.eq(posterior_partition, T.constant(0.0, dtype='float64')), T.constant(0.0, dtype='float64'), partial_sums[-1] / posterior_partition)
-    #data_term = partial_sums[-1]
-    complete_expectation = data_term - T.log(encoder_partition)
-    #complete_expectation = data_term
-    return complete_expectation
   
   def get_sym_nc_complete_expectation(self, x, y_s):
     # NCE function
@@ -171,24 +196,6 @@ class EventAE(object):
     res, _ = theano.scan(fn=get_expectation, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x, y_s])
     complete_expectation = res[-1]
     return complete_expectation
-  
-  def get_sym_nc_direct_prob(self, x, y_s):
-    # NCE function
-    def get_prob(y_0, interm_sum, x_0, Y):
-      posterior = self.get_sym_nc_posterior(x_0, y_0, Y)
-      return interm_sum + posterior
-    res, _ = theano.scan(fn=get_prob, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x, y_s])
-    direct_prob = res[-1]
-    return direct_prob
-    
-  def get_sym_direct_prob(self, x, y_s):
-    def get_post_num_sum(y_0, interm_sum, x_0):
-      posterior_num = self.get_sym_posterior_num(x_0, y_0)
-      return interm_sum + posterior_num
-    res, _ = theano.scan(fn=get_post_num_sum, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x])
-    direct_prob = res[-1] / self.get_sym_encoder_partition(x, y_s)
-    return direct_prob
-  
 
   def get_train_func(self, learning_rate, nce=True, em=False):
     print >>sys.stderr, "Trainining type: EM = %s, NCE = %s"%(em, nce)
@@ -217,6 +224,50 @@ class EventAE(object):
     x, y = T.ivectors('x', 'y')
     rec_prob_func = theano.function([x, y], self.get_sym_rec_prob(x, y))
     return rec_prob_func
+ 
+  ### Relaxed variant functions ### 
+  def get_sym_relaxed_encoder_energy(self, x, y, s):
+    h = self.hyp_model.get_symb_score(x[0], y)
+    p = self.wc_pref_models[s].get_symb_score(x[1], y)
+    return h + p
+
+  def get_sym_relaxed_encoder_partition(self, x, y_s, s):
+    partial_sums, _ = theano.scan(fn=lambda y, interm_sum, x_0: interm_sum + T.exp(self.get_sym_relaxed_encoder_energy(x_0, y, s)), outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x])
+    encoder_partition = partial_sums[-1]
+    return encoder_partition
+
+  def get_sym_relaxed_posterior_num(self, x, y, s):
+    # Needed for NCE
+    enc_energy = self.get_sym_relaxed_encoder_energy(x, y, s)
+    rec_prob = self.rec_model.get_sym_rec_prob(x[0], y)
+    return T.exp(enc_energy) * rec_prob
+      
+  def get_sym_relaxed_posterior_partition(self, x, y_s, s):
+    partial_sums, _ = theano.scan(fn=lambda y, interm_sum, x_0, s: interm_sum + self.get_sym_relaxed_posterior_num(x_0, y, s), outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x,s])
+    posterior_partition = partial_sums[-1]
+    return posterior_partition
+  
+  def get_sym_relaxed_direct_prob(self, x, y_s, s):
+    def get_post_num_sum(y_0, interm_sum, x_0):
+      posterior_num = self.get_sym_relaxed_posterior_num(x_0, y_0, s)
+      return interm_sum + posterior_num
+    res, _ = theano.scan(fn=get_post_num_sum, outputs_info=numpy.asarray(0.0, dtype='float64'), sequences=[y_s], non_sequences=[x])
+    direct_prob = res[-1] / self.get_sym_relaxed_encoder_partition(x, y_s, s)
+    return direct_prob
+
+  def get_relaxed_train_func(self, learning_rate, s):
+    # TODO: Implement AdaGrad
+    # TODO: This means we need one train function per slot.  Do we?
+    x, y_s = T.ivector("x"), T.ivector("y_s")
+    cost = -T.log(self.get_sym_relaxed_direct_prob(x, y_s, s))
+    relaxed_enc_params = self.hyp_model.get_params() + self.wc_pref_models[s].get_params()
+    params = self.repr_params + relaxed_enc_params + self.rec_params
+    g_params = T.grad(cost, params)
+    # Updating the parameters only if the norm of the gradient is less than 100.
+    # Important: This check also takes care of any element in the gradients being nan. The conditional returns False even in that case.
+    updates=[ (p, ifelse(T.le(T.nlinalg.norm(g, None), T.constant(100.0, dtype='float64')), p - learning_rate * g, p)) for p, g in zip(params, g_params) ]
+    train_func = theano.function([x, y_s], cost, updates=updates)
+    return train_func
 
   def set_repr_params(self, repr_param_vals):
     for i, param_val in enumerate(repr_param_vals):
--- run_event_ae.py
+++ run_event_ae.py
@@ -19,6 +19,7 @@ argparser.set_defaults(change_word_rep=False)
 argparser.add_argument('--word_dim', type=int, help="Dimensionality of word representations", default=50)
 argparser.add_argument('--concept_dim', type=int, help="Dimensionality of concept representations", default=50)
 argparser.add_argument('--write_model_freq', type=int, help="Frequency at which the model will be written to disk", default=1)
+argparser.add_argument('--num_slots', type=int, help="Number of slots in the input", default=2)
 argparser.add_argument('--hyp_hidden_size', type=int, help="Hidden layer size in hypernymy if a NN factor is selected", default=20)
 argparser.add_argument('--wc_hidden_size', type=int, help="Hidden layer size in word-concept preferences if a NN factor is selected", default=20)
 argparser.add_argument('--cc_hidden_size', type=int, help="Hidden layer size in concept-concept preferences if a NN factor is selected", default=20)
@@ -26,6 +27,8 @@ argparser.add_argument('--lr', type=float, help="Learning rate", default=0.01)
 argparser.add_argument('--max_iter', type=int, help="Maximum number of iterations", default=100)
 argparser.add_argument('--vocab_file', type=str, help="Word vocabulary file", default="vocab.txt")
 argparser.add_argument('--ont_file', type=str, help="Concept vocabulary file", default="ont.txt")
+argparser.add_argument('--use_relaxation', help="Ignore inter-concept preferences and optimize", action='store_true')
+argparser.set_defaults(use_relaxation=False)
 argparser.add_argument('--use_em', help="Use EM (Default is False)", action='store_true')
 argparser.set_defaults(use_em=False)
 argparser.add_argument('--use_nce', help="Use NCE for estimating encoding probability. (Default is False)", action='store_true')
@@ -42,11 +45,15 @@ if args.pt_rep:
   pt_word_rep = {l.split()[0]: numpy.asarray([float(f) for f in l.strip().split()[1:]]) for l in gzip.open(args.pt_rep)}
 
 dp = DataProcessor(pred_arg_pos)
-x_data, y_s_data, w_ind, c_ind, w_h_map = dp.make_data(args.train_file)
+x_data, y_s_data, w_ind, c_ind, w_h_map = dp.make_data(args.train_file, relaxed=args.use_relaxation)
 rev_w_ind = {ind:word for word, ind in w_ind.items()}
 rev_c_ind = {ind:concept for concept, ind in c_ind.items()}
 
-num_slots = len(x_data[0])
+if not args.use_relaxation:
+  num_slots = len(x_data[0])
+else:
+  num_slots = args.num_slots
+
 num_args = num_slots - 1
 
 wc_hidden_sizes = [args.wc_hidden_size] * num_slots
@@ -72,9 +79,13 @@ vocab_size = len(w_ind)
 ont_size = len(c_ind)
 
 comp_starttime = time.time()
-event_ae = EventAE(num_args, vocab_size, ont_size, args.hyp_hidden_size, wc_hidden_sizes, cc_hidden_sizes, word_dim=args.word_dim, concept_dim=args.concept_dim, word_rep_param=args.change_word_rep, hyp_model_type=args.hyp_model_type, wc_pref_model_type=args.wc_pref_model_type, cc_pref_model_type=args.cc_pref_model_type)
-train_func = event_ae.get_train_func(learning_rate, em=args.use_em, nce=args.use_nce)
-post_score_func = event_ae.get_posterior_func()
+event_ae = EventAE(num_args, vocab_size, ont_size, args.hyp_hidden_size, wc_hidden_sizes, cc_hidden_sizes, word_dim=args.word_dim, concept_dim=args.concept_dim, word_rep_param=args.change_word_rep, hyp_model_type=args.hyp_model_type, wc_pref_model_type=args.wc_pref_model_type, cc_pref_model_type=args.cc_pref_model_type, relaxed=args.use_relaxation)
+
+if args.use_relaxation:
+  train_funcs = [event_ae.get_relaxed_train_func(learning_rate, s) for s in range(num_slots)]
+else:
+  train_func = event_ae.get_train_func(learning_rate, em=args.use_em, nce=args.use_nce)
+  post_score_func = event_ae.get_posterior_func()
 
 if use_pretrained_wordrep:
   num_words_covered = 0
@@ -86,6 +97,7 @@ if use_pretrained_wordrep:
   print >>sys.stderr, "Using pretrained word representations from %s"%(args.pt_rep)
   print >>sys.stderr, "\tcoverage is %f"%(float(num_words_covered)/len(w_ind))
 
+#TODO: Change this to work with relaxation
 def get_mle_y(x_datum, y_s_datum):
   max_score = -float("inf")
   best_y = []
@@ -98,8 +110,8 @@ def get_mle_y(x_datum, y_s_datum):
 comp_endtime = time.time()
 print >>sys.stderr, "Theano compilation took %d seconds"%(comp_endtime - comp_starttime)
 
-x = T.ivector('x') # Vector of word indexes in vocabulary
-y_s = T.imatrix('y_s') # Matrix with all possible concept combinations
+#x = T.ivector('x') # Vector of word indexes in vocabulary
+#y_s = T.imatrix('y_s') # Matrix with all possible concept combinations
 
 print >>sys.stderr, "Starting training"
 for num_iter in range(args.max_iter):
@@ -109,7 +121,11 @@ for num_iter in range(args.max_iter):
   epoch_starttime = time.time()
   for i, (x_datum, y_s_datum) in enumerate(train_data):
     inst_starttime = time.time()
-    cost = train_func(numpy.asarray(x_datum, dtype='int32'), numpy.asarray(y_s_datum, dtype='int32'))
+    if args.use_relaxation:
+      train_func = train_funcs[x_datum[-1]]
+      cost = train_func(numpy.asarray(x_datum[:-1], dtype='int32'), numpy.asarray(y_s_datum, dtype='int32'))
+    else:
+      cost = train_func(numpy.asarray(x_datum, dtype='int32'), numpy.asarray(y_s_datum, dtype='int32'))
     times.append(time.time() - inst_starttime)
     costs.append(cost)
     if (i+1) % 1000 == 0:
@@ -133,10 +149,12 @@ for num_iter in range(args.max_iter):
     wcp_params = [[param.get_value() for param in wcp_model.get_params()] for wcp_model in event_ae.wc_pref_models]
     cPickle.dump(wcp_params, wcp_param_out)
     wcp_param_out.close()
-    ccp_param_out = open("ccp_params_%d.pkl"%(num_iter + 1), "wb")
-    ccp_params = [[param.get_value() for param in ccp_model.get_params()] for ccp_model in event_ae.cc_pref_models]
-    cPickle.dump(ccp_params, ccp_param_out)
-    ccp_param_out.close()
+    if not args.use_relaxation:
+      ccp_param_out = open("ccp_params_%d.pkl"%(num_iter + 1), "wb")
+      ccp_params = [[param.get_value() for param in ccp_model.get_params()] for ccp_model in event_ae.cc_pref_models]
+      cPickle.dump(ccp_params, ccp_param_out)
+      ccp_param_out.close()
+
     rec_param_out = open("rec_params_%d.pkl"%(num_iter + 1), "wb")
     rec_params = [param.get_value() for param in event_ae.rec_params]
     cPickle.dump(rec_params, rec_param_out)
