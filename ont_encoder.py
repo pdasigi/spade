432f466751ab1a6b02a80380aa05edadac08f2bf
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-11-23 18:03:52 -0500

    Ont rep now has representations in the same order as input list

--- ont_encoder.py
+++ ont_encoder.py
@@ -31,16 +31,21 @@ class OntologyEncoder(object):
       for syn_name in syn_names:
         if len(syn_name.split('.')) == 3:
           all_syns.append(wn.synset(syn_name))
+        else:
+          all_syns.append(syn_name)
     else:
       all_syns = wn.all_synsets()
 
     for syn in all_syns:
-      syn_hyponyms = syn.hyponyms()
-      if len(syn_hyponyms) == 0:
+      if type(syn) is str or type(str) is unicode:
         leaves.append(syn)
       else:
-        non_leaves.append(syn)
-        non_leaf_hyps.append((syn, syn_hyponyms))
+        syn_hyponyms = syn.hyponyms()
+        if len(syn_hyponyms) == 0:
+          leaves.append(syn)
+        else:
+          non_leaves.append(syn)
+          non_leaf_hyps.append((syn, syn_hyponyms))
 
     print >>sys.stderr, "Leaves:", len(leaves)
     print >>sys.stderr, "Non leaves:", len(non_leaves)
@@ -69,12 +74,14 @@ class OntologyEncoder(object):
     leaf_syn_index = {}
 
     for syn in leaves + leaf_non_leaves:
-      leaf_syn_index[syn.name()] = len(leaf_syn_index)
-      lemmas = [x.lower() for x in syn.lemma_names()]
+      syn_name = syn if type(syn) is str or type(str) is unicode else syn.name()
+      leaf_syn_index[syn_name] = len(leaf_syn_index)
       lemma_reps = []
-      for lemma in lemmas:
-        if lemma in self.pt_word_rep:
-          lemma_reps.append(self.pt_word_rep[lemma])
+      if not (type(syn) is str or type(syn) is unicode):
+        lemmas = [x.lower() for x in syn.lemma_names()]
+        for lemma in lemmas:
+          if lemma in self.pt_word_rep:
+            lemma_reps.append(self.pt_word_rep[lemma])
       num_lemma_reps = len(lemma_reps)
       num_reps_per_syn.append(num_lemma_reps)
       if num_lemma_reps != 0:
@@ -92,22 +99,24 @@ class OntologyEncoder(object):
     while len(real_non_leaves) != len(non_leaf_reps):
       num_unrepresented_nls = 0
       for nl, known_nl_hyps in real_non_leaves:
-        if nl.name() in non_leaf_syn_index:
+        nl_name = nl if type(nl) is str or type(nl) is unicode else nl.name()
+        if nl_name in non_leaf_syn_index:
           continue
         nl_hyp_reps = []
         unrepresented = False
         for hyp in known_nl_hyps:
-          if hyp.name() in leaf_syn_index:
-            nl_hyp_reps.append(leaf_reps[leaf_syn_index[hyp.name()]])
+          hyp_name = hyp if type(hyp) is str or type(hyp) is unicode else hyp.name()
+          if hyp_name in leaf_syn_index:
+            nl_hyp_reps.append(leaf_reps[leaf_syn_index[hyp_name]])
           elif hyp.name() in non_leaf_syn_index:
-            nl_hyp_reps.append(non_leaf_reps[non_leaf_syn_index[hyp.name()]])
+            nl_hyp_reps.append(non_leaf_reps[non_leaf_syn_index[hyp_name]])
           else:
             num_unrepresented_nls += 1
             unrepresented = True
             break
         if unrepresented:
           continue
-        non_leaf_syn_index[nl.name()] = len(non_leaf_syn_index)
+        non_leaf_syn_index[nl_name] = len(non_leaf_syn_index)
         nl_rep = T.mean(nl_hyp_reps, axis=0)
         non_leaf_reps.append(nl_rep)
       print "Unrepresented: %d"%(num_unrepresented_nls)
@@ -115,17 +124,12 @@ class OntologyEncoder(object):
     ont_index = {}
     ont_rep = []
 
-    for leaf in leaf_syn_index:
-      ind = leaf_syn_index[leaf]
-      ont_index[leaf] = ind
-      ont_rep.append(leaf_reps[ind])
-
-    num_leaves = len(ont_rep)
-
-    for nonleaf in non_leaf_syn_index:
-      ind = non_leaf_syn_index[nonleaf]
-      ont_index[nonleaf] = ind + num_leaves
-      ont_rep.append(non_leaf_reps[ind])
+    for syn_name in syn_names:
+      ont_index[syn_name] = len(ont_index)
+      if syn_name in leaf_syn_index:
+        ont_rep.append(leaf_reps[leaf_syn_index[syn_name]])
+      else:
+        ont_rep.append(non_leaf_reps[non_leaf_syn_index[syn_name]])
 
     ont_rep = theano.shared(ont_rep)
 
@@ -133,7 +137,10 @@ class OntologyEncoder(object):
     print >>sys.stderr, "Average lemmas with reps per syn: %f"%(float(sum(num_reps_per_syn))/len(num_reps_per_syn))
     return leaf_reps, ont_rep, ont_index
 
-syn_names = [x.split()[0] for x in open(sys.argv[2]).readlines()]
-oe = OntologyEncoder(sys.argv[1])
-leafreps, ontreps, ont_index = oe.encode_ont(syn_names)
-print len(ont_index)
+if __name__ == '__main__':
+  syn_names = [x.split()[0] for x in open(sys.argv[2]).readlines()]
+  oe = OntologyEncoder(sys.argv[1])
+  leafreps, ontreps, ont_index = oe.encode_ont(syn_names)
+  for i, syn_name in enumerate(syn_names):
+    assert ont_index[syn_name] == i
+  print len(ont_index)
